1b99fee51e89730289c14571d97de01c6590e7c4
Will Westrop <wwestrop@hotmail.com>
2026-03-01 13:36:20 +0000

    Fix typescript CSS ambient declaration

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,7 +1,7 @@
 // Allow IDE to correctly detect CSS module imports
 declare module '*.module.css' {
-    const classes: { [key: string]: string };
-    export default classes;
+    const classes: { readonly [key: string]: string };
+    export = classes;
 }
 
 declare namespace NodeJS {
